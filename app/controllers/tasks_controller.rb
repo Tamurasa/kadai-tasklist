d56f08ddda70bf7ad827a5764f9175529059268e
satoshi.tamura <ec2-user@ip-172-31-20-201.ec2.internal>
2018-03-06 02:22:28 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,22 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
 
+  def index
+     if logged_in?
+      @user = current_user
+      @task = current_user.tasks.build  # form_for 用
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+     end
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
+
   def create
    @task = current_user.tasks.build(task_params)
     if @task.save
@@ -10,9 +26,25 @@ class TasksController < ApplicationController
     else
       @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'タスクの投稿に失敗しました。'
-      render 'toppages/index'
+      render 'tasks/index'
     end
   end
+  
+   def edit
+    @task = Task.find(params[:id])
+   end
+   
+   def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+   end
 
   def destroy
     @task.destroy
@@ -23,7 +55,7 @@ class TasksController < ApplicationController
    private
 
   def task_params
-    params.require(:micropost).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
    def correct_user
